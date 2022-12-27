8dcda949c4ee4edd9e69ab9009031476ecc81b22
shubhamjoshi99899 <shubhamjoshi99899@gmail.com>
2022-12-27 21:42:27 +0530

    added pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
-  disable: process.env.NODE_ENV === "development",
   dest: "public",
   register: true,
   skipWaiting: false,
